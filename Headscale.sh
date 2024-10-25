250c971e1cb7b4d1be1566ae61db74b25a3e3a57
OmenNef <omennef@gmail.com>
2024-10-25 15:56:58 +0300

    Delete Headscale.sh

--- Headscale.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install vim dialog -y
-wget --output-document=headscale.deb \
-https://github.com/juanfont/headscale/releases/download/v0.23.0/headscale_0.23.0_linux_amd64.deb
-sudo mv /root/headscale.deb /tmp/
-sudo apt install /tmp/headscale.deb
-sudo vim /etc/headscale/config.yaml
