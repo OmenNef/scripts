7b00c8424c3ec350c570b378cdd480f53d8e08fe
OmenNef <omennef@gmail.com>
2024-10-26 10:35:37 +0300

    Add files via upload

--- headscale.sh
+++ headscale.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
+# Устанавливаем expect
+echo "Установка expect..."
+sudo apt update
+sudo apt install -y  expect dialog
+
+# Проверяем, требуется ли перезагрузка сервисов
+echo "Обновление системы и проверка на необходимость перезагрузки сервисов..."
+
+# Используем expect для автоматического ввода
+expect -c '
+    set timeout -1
+
+    # Запускаем команду apt update
+    spawn sudo apt update
+    expect eof
+
+    # Запускаем команду apt upgrade и ожидаем возможный запрос на перезагрузку сервисов
+    spawn sudo apt upgrade -y
+    expect {
+        "Which services should be restarted?" {
+            send "1 2 3 4 5 6 7\r"
+            exp_continue
+        }
+        eof
+    }
+'
+
 # Переключение в корневой каталог
 cd /
 
 # Установка необходимых пакетов
-sudo apt-get install vim dialog -y
+sudo apt-get install vim -y
 
 # Скачивание файла .deb в /tmp
 wget --output-document=/tmp/headscale.deb \
