3bcaa0938474cd9270329b9def419d2412463d92
OmenNef <omennef@gmail.com>
2024-10-26 07:31:41 +0300

    Add files via upload

--- headscale.sh
+++ headscale.sh
@@ -2,5 +2,6 @@
 sudo apt-get install vim dialog -y
 wget --output-document=headscale.deb \
 https://github.com/juanfont/headscale/releases/download/v0.23.0/headscale_0.23.0_linux_amd64.deb
-sudo apt install ./headscale.deb
+mv /root/headscale.deb /tmp/
+sudo apt install /tmp/headscale.deb
 sudo vim /etc/headscale/config.sdyaml
